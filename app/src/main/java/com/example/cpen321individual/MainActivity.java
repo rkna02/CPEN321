bebbf8de34e056b50d997fd4f715c54b129a100c
Peter Na <ruikangna0605@hotmail.com>
2022-06-01 03:58:57 -0700

    added Google sign in button

--- app/src/main/java/com/example/cpen321individual/MainActivity.java
+++ app/src/main/java/com/example/cpen321individual/MainActivity.java
@@ -13,13 +13,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mapsButton;
     private Button modelButton;
     private Button weatherButton;
+    private SignInButton signInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 1;
     final static String TAG = "MainActivity";
     public static int PERMISSIONS_ACCESS_LOCATION = 1;
 
@@ -60,6 +72,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(weatherIntent);
             }
         });
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Google Sign In Button
+        signInButton = findViewById(R.id.sign_in_button);
+        setButtonText(signInButton, "Server Info");
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
     }
 
     // Activity Method
@@ -98,32 +130,78 @@ public class MainActivity extends AppCompatActivity {
             // if user has never seen the permission request
             } else {
                 ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);  // request code usually doesn't matter
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                        && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(MainActivity.this, "Location permissions are needed to show full information", Toast.LENGTH_LONG).show();
-                    new AlertDialog.Builder(this)
-                            .setTitle("Need Location Permissions")
-                            .setMessage("Location permissions are needed to detect current city")
-                            .setNegativeButton("Proceed anyways", new DialogInterface.OnClickListener() {
-                                @Override
-                                // OnClickListener Method, Toast requires Activity method
-                                public void onClick(DialogInterface dialog, int which) {
-                                    Toast.makeText(MainActivity.this, "We need these location permissions to run!", Toast.LENGTH_LONG).show();
-                                    dialog.dismiss();
-                                }
-                            })
-                            .setPositiveButton("Agree", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                                }
-                            })
-                            .create()  // create dialog
-                            .show();
-
-                }
             }
 
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN); // If the sign in works, return RC_SIGN_IN
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account == null) {
+            Log.d(TAG, "There is no user signed in");
+        } else {
+            Log.d(TAG, "Pref Name: " + account.getDisplayName());
+            Log.d(TAG, "Email: " + account.getEmail());
+            Log.d(TAG, "Given Name: " + account.getGivenName());
+            Log.d(TAG, "Family Name: " + account.getFamilyName());
+            Log.d(TAG, "Display URL: " + account.getPhotoUrl());
+
+            // Send token to your back-end
+            // Move to another activity
+
+        }
+    }
+
+    protected void setButtonText(SignInButton signInButton, String text) {
+        // Find the TextView that is inside of the SignInButton and set its text
+        for (int i = 0; i < signInButton.getChildCount(); i++) {
+            View view = signInButton.getChildAt(i);
+
+            if (view instanceof TextView) {
+                TextView textView = (TextView) view;
+                textView.setText(text);
+                return;
+            }
         }
     }
 
